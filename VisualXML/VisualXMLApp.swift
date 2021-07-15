aecc961b7e24e31cc2100607b6ec2eef3aaebf63
Richard Meitzler <rmeitzler@madmobile.com>
2021-07-15 10:54:19 -0400

    Initial Commit

---
+++ VisualXML/VisualXMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualXMLApp.swift
+//  VisualXML
+//
+//  Created by Richard Meitzler on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct VisualXMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
